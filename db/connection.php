c9c5b559429d439c9a460cc079615e4e409ccfcd
VladMeh <vladmeh@gmail.com>
2018-07-06 15:38:57 +0300

    added db credentials

--- db/connection.php
+++ db/connection.php
@@ -9,14 +9,21 @@ class MysqlDB
     const DB="fh-task";
 
     public static function getConnection(){
-        $username = self::USERNAME;
-        $password = self::PASSWORD;
-        $host = self::HOST;
-        $port = self::PORT;
-        $db = self::DB;
+        if ($_SERVER['SERVER_NAME'] == "fh-task-demo.herokuapp.com") {
+            $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+            $host = $url["host"];
+            $username = $url["user"];
+            $password = $url["pass"];
+            $db = substr($url["path"], 1);
+        } else {
+            $username = self::USERNAME;
+            $password = self::PASSWORD;
+            $host = self::HOST;
+            $db = self::DB;
+        }
 
         try {
-            $connection = new PDO("mysql:dbname=$db;host=$host;port=$port", $username, $password, [
+            $connection = new PDO("mysql:dbname=$db;host=$host", $username, $password, [
                 PDO::ATTR_PERSISTENT => true
             ]);
             $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
